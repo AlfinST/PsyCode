1bb42f5b61a77a8e1a7bdb25f46764619255d9f0
AlfinST <alfinsthomas@gmail.com>
2020-06-10 19:23:45 +0530

    Added new not operator

--- Python-XML_Paraser/Stag1JsonImplementation.py
+++ Python-XML_Paraser/Stag1JsonImplementation.py
@@ -191,7 +191,7 @@ def t_BOOLSTRINGS(t):
 	addToLine(("boolStr",t.value))
 
 def t_BOOLOPERATOR(t):
-	r'>=|<=|!=|<|>|==|[Ee]quals|[aA]nd|[oO]r|[Nn]ot\s+equal\s+to'
+	r'>=|<=|!=|<|>|==|[Ee]quals|[aA]nd|[oO]r|[Nn]ot\s+equal\s+to|[Nn]ot'
 	print("<boolOp>",end="")
 	boolDict ={"<":"l",">":"g","==":"eq",\
 				"equals":"eq","<=":"le",">=":"ge",\

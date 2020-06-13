d82bc2ccf002744c1db96d40a7522edda719cc51
AlfinST <alfinsthomas@gmail.com>
2020-06-13 17:26:55 +0530

    Fixed problem with isMultiple

--- Python-XML_Paraser/Stag1JsonImplementation.py
+++ Python-XML_Paraser/Stag1JsonImplementation.py
@@ -125,7 +125,7 @@ def t_ISDIVISIBLE(t):
 def t_ISMULTIPLE(d):
 	r'is\s+a\s+multiple\s+of'
 	print("<boolStr>",end="")
-	addToLine("boolStr","#m")
+	addToLine(("boolStr","#m"))
 
 def t_ISPRIME(t):
 	r'is\s+prime'
